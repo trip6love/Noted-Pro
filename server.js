58bd133bf45a515e69ef97a5ea6830dd471ff0eb
trip6love <lanebyers13@gmail.com>
2021-12-23 19:57:40 -0500

    changed index to server so it will run in the future and added app listen

--- server.js
+++ server.js
@@ -4,3 +4,7 @@ const fs = require('fs');
 const express = require('express');
 const app = express();
 const notesData = require('./db/db.json');
+
+app.listen(3001,() => {
+    console.log ('Server is now live on port 3001');
+});
