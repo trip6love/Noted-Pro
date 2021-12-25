9332d8f64e0d5463a5631bbbd5e8d6d4af5df799
trip6love <lanebyers13@gmail.com>
2021-12-25 00:52:02 -0500

    Added get for api routes now going on to the function to create the notes

--- server.js
+++ server.js
@@ -10,7 +10,19 @@ app.use(express.json());
 app.use(express.static('public'));
 
 // Add API routes //
+app.get('/api/notes', (req, res) => {
+    res.json(notesData.slice(1));
+});
 
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/notes.html'));
+});
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+// Add function to create a new note //
 
 
 app.listen(3001,() => {
