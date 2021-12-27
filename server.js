c5a1a567bf75ffaf4ffc33703ee6b2134538bb6b
trip6love <lanebyers13@gmail.com>
2021-12-26 21:40:05 -0500

    fixed bug where i couldnt click and navigate to note

--- server.js
+++ server.js
@@ -3,24 +3,25 @@ const path = require('path');
 const fs = require('fs');
 const express = require('express');
 const app = express();
-const notesData = require('./db/db.json');
+let notesData = require('./db/db.json');
 const { v4: uuidv4 } = require('uuid');
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
 
+// Add API routes //
+app.get('/api/notes', (req, res) => {
+    let data = notesData;
+    res.json(data);
+});
+
 app.post('/api/notes', (req, res) => {
     req.body.id = uuidv4();
     notesData.push(req.body);
-    fs.writeFileSync('./db/db.json', JSON.stringify(notes, null, '/t'));
+    fs.writeFileSync('./db/db.json', JSON.stringify(notesData, null, '/t'));
     res.json(notesData);
 })
-
-// Add API routes //
-app.get('/api/notes', (req, res) => {
-    res.json(notesData.slice(1));
-});
 
 app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, './public/notes.html'));
