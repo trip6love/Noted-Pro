9a480fb84bd895feb0b00881828f01993f671b1d
trip6love <lanebyers13@gmail.com>
2021-12-26 21:29:33 -0500

    adding new post method

--- server.js
+++ server.js
@@ -10,6 +10,13 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
 
+app.post('/api/notes', (req, res) => {
+    req.body.id = uuidv4();
+    notesData.push(req.body);
+    fs.writeFileSync('./db/db.json', JSON.stringify(notes, null, '/t'));
+    res.json(notesData);
+})
+
 // Add API routes //
 app.get('/api/notes', (req, res) => {
     res.json(notesData.slice(1));
@@ -28,31 +35,31 @@ app.get('/', (req, res) => {
 });
 
 // Add function to create a new note //
-function addnewNote(body, array) {
-    const newNote = body;
+//function addnewNote(body, array) {
+    //const newNote = body;
     
-    if (!Array.isArray(array))
-        array = [];
+    //if (!Array.isArray(array))
+        //array = [];
     
-    if (array.length === 0)
-        array.push(0);
+    //if (array.length === 0)
+        //array.push(0);
     
-    body.id = array[0];
-    array[0]++;
+   // body.id = array[0];
+    //array[0]++;
 
-    array.push(newNote);
-    fs.writeFileSync(
-        path.join(__dirname, './db/db.json'),
-        JSON.stringify(array, null, 2)
-    );
-    return newNote;
-};
+    //array.push(newNote);
+    //fs.writeFileSync(
+        //path.join(__dirname, './db/db.json'),
+        //JSON.stringify(array, null, 2)
+    //);
+    ////return newNote;
+///};
 
 // Add a post //
-app.post('/api/notes', (req, res) => {
-    const newNote = addnewNote(req.body, notesData);
-    res.json(newNote);
-});
+//app.post('/api/notes', (req, res) => {
+    //const newNote = addnewNote(req.body, notesData);
+    //res.json(newNote);
+//});
 
 // Listening to PORT and logging the in use server number //
 app.listen(PORT,() => {
